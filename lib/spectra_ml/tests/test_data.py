c9c64ea0057a4f6b0b888ae694e68258fcb385be
Srikar M <srikar@velexi.com>
2019-07-29 17:27:52 -0700

    Add unit test 2

--- lib/spectra_ml/tests/test_data.py
+++ lib/spectra_ml/tests/test_data.py
@@ -24,56 +24,56 @@ class data_tests(unittest.TestCase):  # pylint: disable=invalid-name
     """
     # --- Test preparation and clean up
 
-#    def setUp(self):  # pylint: disable=invalid-name
-#        """
-#        Perform preparations required by most tests.
-#
-#        - Get spectrum for tests
-#        """
-#        # Set data paths
-#        data_dir = os.environ['DATA_DIR']
-#        spectrometers_dir = os.path.join(data_dir, 'spectrometers')
-#        splib07a_dir = os.path.join(data_dir, 'ASCIIdata_splib07a')
-#
-#        # Get spectrometer for tests
-#        spectrometers = io.load_spectrometers(spectrometers_dir, splib07a_dir)
-#        spectrometer = spectrometers['ASDFR']
-#
-#        # Get spectrum for tests
-#        spectrum_path = os.path.join(
-#            splib07a_dir, 'ChapterM_Minerals',
-#            'splib07a_Stilbite_HS482.1B_Zeolite_ASDFRb_AREF.txt')
-#
-#        self.spectrum, _ = io.load_spectrum(spectrum_path, spectrometer)
-#
-#    # --- Test cases
-#
-#    def test_resample_spectrum_1(self):
-#        """
-#        Test resample_spectrum(). Normal usage.
-#        """
-#        # --- Preparations
-#
-#        # Set wavelengths to resample at
-#        num_wavelengths = 100
-#        wavelength_min = min(self.spectrum.index)
-#        wavelength_max = max(self.spectrum.index)
-#        wavelengths = pd.Series(np.linspace(wavelength_min, wavelength_max,
-#                                            num_wavelengths))
-#
-#        # --- Exercise functionality and check results
-#
-#        resampled_spectrum = \
-#            data.resample_spectrum(self.spectrum, wavelengths)
-#
-#        # Check data structure
-#        assert isinstance(resampled_spectrum, pd.DataFrame)
-#        assert resampled_spectrum.index.name == 'wavelength'
-#        assert (resampled_spectrum.index == wavelengths).all()
-#        assert len(resampled_spectrum) == len(wavelengths)
-#
-#        # Check data values
-#        assert (resampled_spectrum['reflectance'] >= 0).all()
+    def setUp(self):  # pylint: disable=invalid-name
+        """
+        Perform preparations required by most tests.
+
+        - Get spectrum for tests
+        """
+        # Set data paths
+        data_dir = os.environ['DATA_DIR']
+        spectrometers_dir = os.path.join(data_dir, 'spectrometers')
+        splib07a_dir = os.path.join(data_dir, 'ASCIIdata_splib07a')
+
+        # Get spectrometer for tests
+        spectrometers = io.load_spectrometers(spectrometers_dir, splib07a_dir)
+        spectrometer = spectrometers['ASDFR']
+
+        # Get spectrum for tests
+        spectrum_path = os.path.join(
+            splib07a_dir, 'ChapterM_Minerals',
+            'splib07a_Stilbite_HS482.1B_Zeolite_ASDFRb_AREF.txt')
+
+        self.spectrum, _ = io.load_spectrum(spectrum_path, spectrometer)
+
+    # --- Test cases
+
+    def test_resample_spectrum_1(self):
+        """
+        Test resample_spectrum(). Normal usage.
+        """
+        # --- Preparations
+
+        # Set wavelengths to resample at
+        num_wavelengths = 100
+        wavelength_min = min(self.spectrum.index)
+        wavelength_max = max(self.spectrum.index)
+        wavelengths = pd.Series(np.linspace(wavelength_min, wavelength_max,
+                                            num_wavelengths))
+
+        # --- Exercise functionality and check results
+
+        resampled_spectrum = \
+            data.resample_spectrum(self.spectrum, wavelengths)
+
+        # Check data structure
+        assert isinstance(resampled_spectrum, pd.DataFrame)
+        assert resampled_spectrum.index.name == 'wavelength'
+        assert (resampled_spectrum.index == wavelengths).all()
+        assert len(resampled_spectrum) == len(wavelengths)
+
+        # Check data values
+        assert (resampled_spectrum['reflectance'] >= 0).all()
 
     def test_resample_spectrum_2(self):
         """
