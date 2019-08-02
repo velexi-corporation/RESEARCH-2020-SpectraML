4ab3d8a177e6241066365c12014627fcc452a3d0
fromhello2hello <bonita@velexi.com>
2019-08-02 14:43:45 -0700

    working fc raw bootstrap

--- lib/spectra_ml/metrics.py
+++ lib/spectra_ml/metrics.py
@@ -95,3 +95,68 @@ def bootstrap(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
         bresults[idevresults,run] = dev_acc
 
     return bresults
+
+def bootstrap2(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
+# --- bootstrap loop
+
+# -select bootstrap sets
+# -run algorithm
+# -repeat num_bootstrap_runs times
+# -collect mean and variance of train dev accuracies
+# and store in bresults for run in range(num_bootstrap_runs)
+
+    #read data
+    spectra = X                 #name support
+    num_samples = X.shape[0]
+
+    #y_old = y
+    #y = keras.utils.to_categorical(y)
+
+    #split data into populations
+    itrain,idev,itest = split(num_samples)
+    train_population_indices = itrain   #name support
+    dev_population_indices = idev        #name support
+    dev_set_indices = dev_population_indices
+    BATCH_SIZE=batch_size               #name support
+    EPOCHS = num_epochs                 #name support
+
+    #array for results
+    num_tests = 2                       #train, dev
+    itrainresults = 0                          #row index of train results
+    idevresults = 1                            #row index of dev results
+    bresults = np.zeros((num_tests, num_bootstrap_runs))
+
+    ## TODO:
+    #handle model
+
+    for run in range(num_bootstrap_runs):
+        # - bootstrap sample the populations to make sets
+        # make train, dev, and test sets from their respective populations
+
+        # draw with replacement from the train population
+        # make the validation and test sets the same as their populations
+        train_set_indices = random.choices(train_population_indices, k=len(train_population_indices))
+        dev_set_indices = dev_population_indices
+
+        # make train and test sets
+        train_set = spectra[train_set_indices, :]
+        train_set_labels = y[train_set_indices, :]
+        dev_set = spectra[dev_set_indices, :]
+        dev_set_labels = y[dev_set_indices,:]
+
+        #train_set = np.reshape(train_set, (train_set.shape[0], train_set.shape[1], 1))
+        #dev_set = np.reshape(dev_set, (dev_set.shape[0], train_set.shape[1], 1))
+
+
+        # train
+        History = model.fit(train_set, train_set_labels, batch_size=BATCH_SIZE,\
+         epochs=EPOCHS, verbose=1, validation_data=(dev_set, dev_set_labels))
+        acc = History.history['acc']
+        bresults[itrainresults, run] = acc[num_epochs-1]
+        #record final epoch, plain nn train result, acc[...]), for each run
+
+        # test on dev set
+        dev_loss, dev_acc = model.evaluate(dev_set, dev_set_labels)
+        bresults[idevresults,run] = dev_acc
+
+    return bresults
