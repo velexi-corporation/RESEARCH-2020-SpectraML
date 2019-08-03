aadf9ef231333b9c3f3becd235f37b2a0c3066d0
fromhello2hello <bonita@velexi.com>
2019-08-02 17:32:08 -0700

    rename, implement test accuracy on fc raw, 1d raw

--- lib/spectra_ml/metrics.py
+++ lib/spectra_ml/metrics.py
@@ -12,6 +12,12 @@ import tensorflow as tf
 from tensorflow import keras
 import keras
 
+def addchannel(X):
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+    #train_set = np.reshape(train_set, (train_set.shape[0], train_set.shape[1], 1))
+    #dev_set = np.reshape(dev_set, (dev_set.shape[0], train_set.shape[1], 1))
+    return X
+
 def split(num_samples):
     sample_indices = list(range(0, num_samples))
     random.seed(0)
@@ -26,11 +32,33 @@ def split(num_samples):
     return (train_population_indices, dev_population_indices, test_population_indices)
 
 #TODO
-def aggregate():
-    test = 3
+def test(model,X,y,num_epochs, batch_size):
+    spectra = X                 #name support
+    num_samples = X.shape[0]
+
+    X_old = X
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+    #y_old = y
+    #y = keras.utils.to_categorical(y)
+
+    #split data into populations
+    itrain,idev,itest = split(num_samples)
+    train_population_indices = itrain   #name support
+    dev_set = idev                      #name support
+    BATCH_SIZE=batch_size               #name support
+    EPOCHS = num_epochs                 #name support
+
+    History = model.fit(train_set, train_set_labels, batch_size=BATCH_SIZE,\
+    epochs=EPOCHS, verbose=1, validation_data=(dev_set, dev_set_labels))
+    acc = History.history['acc']
+    bresults[itrainresults, run] = acc[num_epochs-1]
+    #record final epoch, plain nn train result, acc[...]), for each run
+
+    # test on dev set
+    dev_loss, dev_acc = model.evaluate(dev_set, dev_set_labels)
+    test_loss, test_acc = model.evaluate(test_set, test_set_labels)
     return test
 
-#TODO
 def bootstrap(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
 # --- bootstrap loop
 
@@ -50,8 +78,7 @@ def bootstrap(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
     #split data into populations
     itrain,idev,itest = split(num_samples)
     train_population_indices = itrain   #name support
-    dev_population_indices = idev        #name support
-    dev_set_indices = dev_population_indices
+    dev_set_labels           = idev        #name support
     BATCH_SIZE=batch_size               #name support
     EPOCHS = num_epochs                 #name support
 
@@ -120,7 +147,7 @@ def bootstrap2(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
     BATCH_SIZE=batch_size               #name support
     EPOCHS = num_epochs                 #name support
 
-    #array for results
+    #array for bootstrap results
     num_tests = 2                       #train, dev
     itrainresults = 0                          #row index of train results
     idevresults = 1                            #row index of dev results
@@ -160,3 +187,7 @@ def bootstrap2(model,X,y,num_epochs, batch_size, num_bootstrap_runs):
         bresults[idevresults,run] = dev_acc
 
     return bresults
+
+#TODO
+def bstats(bresults_array):
+    return 5
